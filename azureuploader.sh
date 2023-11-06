86cad7f7411a969ee27af862647724287607d2be
miiandyuu <raston3232@gmail.com>
2023-11-07 04:07:41 +0700

    Fix upload item with ' '(space) on item name

--- azureuploader.sh
+++ azureuploader.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Default values for options
 ACCOUNT_NAME=""
 CONTAINER_NAME=""
 BLOB_NAME=""
@@ -8,16 +9,18 @@ FILE_PATH=()
 while getopts ":a:c:b:f:" opt; do
     case $opt in
         a)
-            ACCOUNT_NAME=$OPTARG
+            ACCOUNT_NAME="$OPTARG"
             ;;
         c)
-            CONTAINER_NAME=$OPTARG
+            CONTAINER_NAME="$OPTARG"
             ;;
         b)
-            BLOB_NAME=$OPTARG
+            BLOB_NAME="$OPTARG"
             ;;
         f)
-            FILE_PATH=$OPTARG
+            FILE_PATH+=("$OPTARG")
+            # IFS =' ' read -r -a FILE_PATH <<< "$OPTARG"
+            # echo $FILE_PATH
             ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
@@ -30,27 +33,37 @@ while getopts ":a:c:b:f:" opt; do
     esac
 done
 
+# Check for Azure CLI authentication
 if ! az account show &>/dev/null; then
     echo "Azure CLI is not authenticated. Please run 'az login' to authenticate."
     exit 1
 fi
 
-if [ -z "$ACCOUNT_NAME"] || [ -z "$CONTAINER_NAME"] || [ ${#FILE_PATH[@]} -eq 0 ]; then
+# Check if the required options are provided
+if [ -z "$ACCOUNT_NAME" ] || [ -z "$CONTAINER_NAME" ] || [ ${#FILE_PATH[@]} -eq 0 ]; then
     echo "Usage: $0 -a <account-name> -c <container-name> -f <file-path> [-b <blob_name>]"
     exit 1
 fi
 
-if [ ! -f "$FILE_PATH" ]; then
-    echo "File not found: $FILE_PATH"
-    exit 1
-fi
+# IFS=' ' read -ra FILE_PATH <<< "$FILE_PATH"
 
-BLOB_NAME=${BLOB_NAME:-$(basename $FILE_PATH)}
+for FILE in "${FILE_PATH[@]}"; do
+    #Check if the file exists
+    if [ ! -f "$FILE" ]; then
+        echo "File not found: $FILE"
+        continue
+    fi
 
-az storage blob upload --account-name $ACCOUNT_NAME --container-name $CONTAINER_NAME --name $BLOB_NAME --file $FILE_PATH --auth-mode login
+    # Use the provided BLOB
+    BLOB_NAME=${BLOB_NAME:-$(basename "$FILE")}
 
-if [ $? -eq 0 ]; then
-    echo "File uploaded successfully."
-else
-    echo "File upload failed."
-fi
+    # Upload the file to Azure Blob Storage
+    az storage blob upload --account-name "$ACCOUNT_NAME" --container-name "$CONTAINER_NAME" --name "$BLOB_NAME" --file "$FILE" --auth-mode login
+
+    # Check for the upload status
+    if ! az storage blob upload &>/dev/null; then
+        echo "File '$FILE' uploaded successfully as '$BLOB_NAME'."
+    else
+        echo "Error uploading the file '$FILE'."
+    fi
+done
